aaa41a2d616be8436c4797b5c120f755921d266d
Taewoo Ki <ilml2@daum.net>
2023-12-07 11:36:19 +0900

    Updated Ex01

--- Project/src/com/javalec/ex01/Ex01.java
+++ Project/src/com/javalec/ex01/Ex01.java
@@ -2,18 +2,25 @@ package com.javalec.ex01;
 
 public class Ex01 {
 
+	/*	
+	Description : 10의 10의 0승에서 ~ 10의 10승까지의 제곱승을 구하라.
+	Date : 23.12.06
+	Version : 1.0
+	Author : 기태우
+	*/
 
 		
 	public Ex01() {
 	
-		resultEx01();
+		(resultEx01)
+		
 	}
 	
 	
 	public void resultEx01() {	
-	for (long i = 0, num = 1; i <= 10;i++) {
-		num *=10; 
-	System.out.println("10^" + String.format("%2d", i) + " =" + String.format("%13d", num/10));
+	for (long i = 0, num = 1; i <= 10;i++) { 
+		num *=10; 		// 10의 재곱을 더하는 식.
+	System.out.println("10^" + String.format("%2d", i) + " =" + String.format("%13d", num/10)); // 10으로 나누어 출력하기.
 			}
 	
 		
